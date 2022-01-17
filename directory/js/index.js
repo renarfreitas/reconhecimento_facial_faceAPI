9a00259b045594ff13dc6e2cb64bd60da7132ff3
Renar Freitas dos Santos <38799859+renarfreitas@users.noreply.github.com>
2022-01-16 21:27:12 -0300

    Projeto abandonado.

--- directory/js/index.js
+++ directory/js/index.js
@@ -1,10 +1,19 @@
+const cam = document.getElementById('cam')
+
 navigator.mediaDevices.enumerateDevices()
     .then(devices => {
        if (Array.isArray(devices)) {
            devices.forEach(device => {
+               console.log(device)
                if(device.kind === 'videoinput'){
                     if(device.groupId.includes('419a41b02cb0c6fac23fa8fc3757eed3d5ce43da926f6519acbe4a7f256bebda')){
-                        
+                        navigator.getUserMedia(
+                            { video: {
+                                deviceId: device.groupId
+                            }},
+                            stream => cam.srcObject = stream,
+                            error => console.error(error)
+                        )
                     }
                }
            })
